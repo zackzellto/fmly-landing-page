fc9f78d4e6ecedb4215a2d7ce9f83b3121c472f7
Zack Zellto <zacharyzellto@gmail.com>
2023-03-21 10:21:08 -0600

    tested mobile view, --looks good and responsive

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,11 @@
-import express, { Request, Response } from 'express';
+require('esm')(module);
+import express from 'express';
 import path from 'path';
 
-const app = express();
+// Create a new express application instance
+const app: express.Application = express();
+
+
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -10,38 +14,41 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 // Define a route that returns a JSON response
-app.get('/api/waitlist', (req: Request, res: Response) => {
+app.get('/api/waitlist', (req, res) => {
+  req.get('Content-Type');  // => "application/json"
   res.json({ message: 'Hello, World!' });
 });
 
 // Define a route that returns a JSON response
-app.get('/api/waitlist/id', (req: Request, res: Response) => {
+app.get('/api/waitlist/id', (req, res) => {
+  req.get('Content-Type');  // => "application/json"
   res.json({ message: 'Hello, World!' });
 });
 
 // Define a route that handles POST requests
-app.post('/api/waitlist', (req: Request, res: Response) => {
+app.post('/api/waitlist', (req, res) => {
   const data = req.body;
   console.log(data);
   res.json({ message: 'Email Received!' });
 });
 
 // Define a route that handles DELETE requests
-app.delete('/api/waitlist/id', (req: Request, res: Response) => {
+app.delete('/api/waitlist/id', (req, res) => {
   const data = req.body;
   console.log(data);
   res.json({ message: 'Email Deleted!' });
 });
 
 // Define a route that handles PUT requests
-app.put('/api/waitlist/id', (req: Request, res: Response) => {
+app.put('/api/waitlist/id', (req, res) => {
   const data = req.body;
   console.log(data);
   res.json({ message: 'Email Updated!' });
 });
 
 // Start the server
-const PORT = process.env.SERVER_PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+const port = 5000;
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${port}/`);
 });
